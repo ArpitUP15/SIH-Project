d02d11d5818e58ad76b895f3391bcdd5b0667858
Mohitgoswami18 <mohitgoswami18326@gmail.com>
2025-09-14 08:54:30 +0530

    Initialize frontend project with React, Vite, and Tailwind CSS

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-red-500'>
+      Testing the running environment of the git and project structure
+    </div>
+  )
+}
+
+export default App
